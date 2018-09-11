174af06ed98d80e36118f22141df9bbee4edfb79
curiositypaths <curiositypaths@users.noreply.github.com>
2018-09-10 20:48:50 -0400

    Complete Accounts exercise

--- exercises/accounts/src/account-manager.ts
+++ exercises/accounts/src/account-manager.ts
@@ -1,15 +1,19 @@
-interface IUser {
+interface User {
   email: string;
   password: string;
-  isActive: boolean;
+  
 }
 
-interface IAdmin {
-  adminSince: Date;
+interface ConfirmedUser extends  User {
+  isActive: true
+}
+
+interface Admin extends ConfirmedUser {
+  adminSince: Date
 }
 
 export class AccountManager {
-  users = new Array();
+  users: User[] = new Array();
 
   /**
    * Create a new user account
@@ -18,7 +22,7 @@ export class AccountManager {
    * @return the new user account. An admin must activate it using activateNewUser
    * @see this.activateNewUser
    */
-  register(email, password) {
+  register(email: string, password: string): User {
     if(!email) throw 'Must provide an email';
     if(!password) throw 'Must provide a password';
     let user = { email, password };
@@ -32,10 +36,11 @@ export class AccountManager {
    * @param userToApprove Newly-registered user, who is to be activated
    * @return the updated user object, now activated
    */
-  activateNewUser(approver, userToApprove) {
+  activateNewUser(approver: Admin, userToApprove: User) {
     if (!approver.adminSince) throw "Approver is not an admin!";
-    userToApprove.isActive = true;
-    return userToApprove;
+    let toConfirm = userToApprove as ConfirmedUser
+    toConfirm.isActive = true;
+    return toConfirm;
   }
 
   /**
@@ -44,10 +49,11 @@ export class AccountManager {
    * @param user an active user who you're making an admin
    * @return the updated user object, now can also be regarded as an admin
    */
-  promoteToAdmin(existingAdmin, user) {
+  promoteToAdmin(existingAdmin: Admin, user: ConfirmedUser) {
     if (!existingAdmin.adminSince) throw "Not an admin!";
     if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
-    user.adminSince = new Date();
-    return user;
+    let adminToConfirm = user as Admin
+    adminToConfirm.adminSince = new Date();
+    return adminToConfirm;
   }
 }
